813fea2c6da58de49c9cf1c1e843765403c1016c
Mohamed HABIB ALLAH <habibsimo692@gmail.com>
2022-11-09 00:11:21 +0100

    Update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,75 @@
 /*   By: mhabib-a <mhabib-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 20:42:41 by mhabib-a          #+#    #+#             */
-/*   Updated: 2022/11/06 20:42:42 by mhabib-a         ###   ########.fr       */
+/*   Updated: 2022/11/08 23:54:11 by mhabib-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include"get_next_line.h"
+#define BUFFER_SIZE 1
+
+char    *ft_read1(int fd, char *s)
+{
+    ssize_t sz;
+    char    *buff;
+    char    *line;
+    
+    buff = malloc(sizeof(char) * BUFFER_SIZE);
+    if (!buff)
+        return (NULL);
+    sz = read(fd, buff, BUFFER_SIZE);
+    while (sz)
+    {
+        buff[sz] = 0;
+        if(!s)
+            s = ft_strdup(buff);
+        else 
+            s = ft_strjoin(s, buff);
+        if(ft_strchr(s, '\n'))
+            break;
+        sz = read(fd, buff, BUFFER_SIZE);
+    }
+    return (s);
+}
+
+char    *ft_ol(char *ltr)
+{
+    char    *line;
+    size_t  i = 0;
+
+    while(ltr[i] != '\n')
+        i++;
+    line = malloc(sizeof(char) * (i + 1));
+    if (!line)
+        return (NULL);
+    i = 0;
+    while (ltr[i]  != '\n')
+    {
+        line[i] = ltr[i];
+        i++;
+    }
+    line[i] = '\n';
+    
+    return(line);
+}
+
+char    *get_next_line(int fd)
+{
+    static char *ltr;
+    char        *line;
+
+    ltr = ft_read1(fd,ltr);
+    line = ft_ol(ltr);
+    printf("%s",line);
+    return(line);
+}
+
+int main()
+{
+    int fd;
+    char *str;
+    fd = open("mohamed", O_RDONLY);
+    str = get_next_line(fd);
+  //  printf("%s",str);    
+    return(0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,22 @@
 /*   By: mhabib-a <mhabib-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 20:42:43 by mhabib-a          #+#    #+#             */
-/*   Updated: 2022/11/06 20:42:44 by mhabib-a         ###   ########.fr       */
+/*   Updated: 2022/11/08 21:52:33 by mhabib-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+char    *ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+size_t ft_strlen(const char *s);
+
+#endif
